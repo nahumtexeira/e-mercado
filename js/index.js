0ef615f64fd600ff98ece627e7973ad7949da743
dlescano2 <140979194+dlescano2@users.noreply.github.com>
2023-08-17 10:31:45 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -4,15 +4,15 @@ function usuarioAutenticado() {
     return localStorage.getItem("autenticado") === "true";
 }
 
-if (!usuarioAutenticado && !window.location.href.endsWith("login.html")) {
-    window.location.href = "login.html";
+if (!usuarioAutenticado && !window.location.href.endsWith("e-mercado/login.html")) {
+    window.location.href = "e-mercado/login.html";
 }
 
 localStorage.removeItem("autenticado");
 
 document.getElementById("cerrarSesion").addEventListener("click", function() {
     localStorage.removeItem("autenticado");
-    window.location.href = "login.html";
+    window.location.href = "e-mercado/login.html";
 });
 
 

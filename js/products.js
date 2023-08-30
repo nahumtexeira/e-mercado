9bf94b9a7fafa0412d804501b86fff1265250b5f
Matias Dutra <114629823+MatiasDutra23@users.noreply.github.com>
2023-08-30 11:32:30 -0300

    adicionada funcionalidad price max min
    se le agrego funcionalidad al filtro price min y pricemax, además de una función para limpiar los filtros

--- js/products.js
+++ js/products.js
@@ -1,3 +1,4 @@
+// Obtener elementos del HTML
 const categoryID = localStorage.getItem("catID");
 const DATA_URL = "https://japceibal.github.io/emercado-api/cats_products/" + categoryID + ".json";
 
@@ -7,15 +8,20 @@ const sortAscRadio = document.getElementById("sortAsc");
 const sortDescRadio = document.getElementById("sortDesc");
 const sortByCountRadio = document.getElementById("sortByCount");
 
+const rangeFilterMinInput = document.getElementById("rangeFilterCountMin");
+const rangeFilterMaxInput = document.getElementById("rangeFilterCountMax");
+const rangeFilterButton = document.getElementById("rangeFilterCount");
+const clearFilterButton = document.getElementById("clearRangeFilter");
+
 // Variable para almacenar los datos
 let dataArray = [];
 let sortByPriceAsc = true; // Variable para rastrear el orden ascendente/descendente por precio
 
 // Función para mostrar los datos en el contenedor
-function showData() {
+function showData(data) {
   products.innerHTML = ""; // Limpiar el contenedor antes de mostrar los datos
 
-  for (const item of dataArray) {
+  for (const item of data) {
     products.innerHTML += `
       <div class="productCard"> 
           <img src="${item.image}" alt="Product Image"> 
@@ -35,20 +41,52 @@ function showData() {
 // Función para ordenar los datos por precio de forma ascendente
 function sortDataByPriceAsc() {
   dataArray.sort((a, b) => a.cost - b.cost);
-  showData();
+  showData(dataArray);
 }
 
 // Función para ordenar los datos por precio de forma descendente
 function sortDataByPriceDesc() {
   dataArray.sort((a, b) => b.cost - a.cost);
-  showData();
+  showData(dataArray);
 }
 
 // Función para ordenar los datos por cantidad de vendidos
 function sortDataBySoldCount() {
   dataArray.sort((a, b) => b.soldCount - a.soldCount);
-  showData();
+  showData(dataArray);
 }
+
+// Función para filtrar por rango de precio
+function filterByPriceRange(minPrice, maxPrice) {
+  return dataArray.filter(item => {
+    const itemPrice = item.cost;
+
+    if (minPrice !== "" && maxPrice !== "") {
+      return itemPrice >= minPrice && itemPrice <= maxPrice;
+    } else if (minPrice !== "") {
+      return itemPrice >= minPrice;
+    } else if (maxPrice !== "") {
+      return itemPrice <= maxPrice;
+    }
+
+    return true; // Si no hay filtros, mantener el producto
+  });
+}
+
+// Agregar manejadores de evento para los botones
+rangeFilterButton.addEventListener("click", () => {
+  const minPrice = rangeFilterMinInput.value;
+  const maxPrice = rangeFilterMaxInput.value;
+
+  const filteredData = filterByPriceRange(minPrice, maxPrice);
+  showData(filteredData);
+});
+
+clearFilterButton.addEventListener("click", () => {
+  rangeFilterMinInput.value = "";
+  rangeFilterMaxInput.value = "";
+  showData(dataArray); // Mostrar todos los productos al limpiar filtros
+});
 
 // Cargar y mostrar datos iniciales
 fetch(DATA_URL)
@@ -56,7 +94,7 @@ fetch(DATA_URL)
   .then(data => {
     dataArray = data.products;
     categoria.innerHTML = data.catName;
-    showData();
+    showData(dataArray);
   });
 
 // Agregar manejadores de evento para los botones de orden
